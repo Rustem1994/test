bc3aeb0c8ec86519f87f8161ca0df779a61130c4
Win10Pro <test@test.ru>
2022-03-16 21:49:55 +0300

    message

--- src/Test.java
+++ src/Test.java
@@ -1,14 +1,17 @@
 public class Test {
      int H=0;
      public Test(int H, boolean flag){
+
          this.H=H;
      }
 
      public void print(){
+
          System.out.println("H="+H);
      }
 
      private void print1(){
+
          System.out.println("H="+H);
      }
 }
