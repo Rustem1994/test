520c64a653fb9659189f6c9883addf422231345f
Win10Pro <test@test.ru>
2022-03-16 21:36:10 +0300

    message

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Test H=new Test(5, true);
+        H.print();
+        H.print();
+	// write your code here
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,14 @@
+public class Test {
+     int H=0;
+     public Test(int H, boolean flag){
+         this.H=H;
+     }
+
+     public void print(){
+         System.out.println("H="+H);
+     }
+
+     private void print1(){
+         System.out.println("H="+H);
+     }
+}
