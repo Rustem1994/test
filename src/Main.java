0819595f400a914332ae3a5c2a1d1fd11b569d3c
Win10Pro <test@test.ru>
2022-04-08 22:24:21 +0300

    Message

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,55 @@
+import java.io.*;
+
+/*
+На вход пользоваль указывает имя файла
+Если файл не нашли, то открываем run.ini
+ */
 public class Main {
 
-    public static void main(String[] args) {
-        Test H=new Test(5, true);
-        H.print();
-        H.print();
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println(args.length);
+        if (args.length>=1){
+            System.out.println(args[0]);
+            FileInputStream fileInputStream = null;
+            /*try {
+                if ((new File(args[0])).exists()){
+                    System.out.println("Файл есть");
+                    fileInputStream = new FileInputStream(args[0]);
+                }else{
+                    fileInputStream = new FileInputStream("run.ini");
+                }
 
+            } catch (FileNotFoundException e) {
+                System.out.println("Ошибка при открытии файла "+args[0]+". Указанный в параметре args[0]");
+                e.printStackTrace();
+             //   fileInputStream = new FileInputStream("run.ini");
+            }*/
+
+            int index=0;
+            try (BufferedReader br = new BufferedReader(new FileReader(args[0])))
+            {
+               // BufferedReader br = new BufferedReader(new FileReader(args[0]));
+                String sCurrentLine;
+                while ((sCurrentLine = br.readLine()) != null)
+                {
+                    index++;
+                    System.out.println(sCurrentLine);
+                }
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+
+            System.out.println("В файле "+index+" строк");
+
+          /*  int i;
+
+            while((i=fileInputStream.read())!= -1){
+
+                System.out.print((char)i);
+            }*/
+        }
 	// write your code here
     }
 }
